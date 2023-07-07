bbadb057e5babf241a7b89d51a485d78f967e643
AlbertoJimenezFiccion <138886381+AlbertoJimenezFiccion@users.noreply.github.com>
2023-07-07 16:46:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Generar ubicación aleatoria del pedo invisible
 var targetLocation = Math.floor(Math.random() * 100) + 1;
 var attempts = 0;
 
@@ -6,16 +5,32 @@ function checkGuess() {
   var guessInput = document.getElementById("guessInput");
   var message = document.getElementById("message");
   var guess = parseInt(guessInput.value);
-  var errorSound = new Audio("wet-fart-6139.mp3"); 
 
-function playErrorSound() {
-  errorSound.play();
-}
+  var errorSound = new Audio("wet-fart-6139.mp3");
 
-  
+  function playErrorSound() {
+    errorSound.play();
+  }
+
   if (isNaN(guess) || guess < 1 || guess > 100) {
-  message.textContent = "Por favor, ingresa un número válido entre 1 y 100.";
-  playErrorSound();
+    message.textContent = "Por favor, ingresa un número válido entre 1 y 100.";
+    playErrorSound();
+  } else {
+    attempts++;
+
+    if (guess < targetLocation) {
+      message.textContent = "¡El pedo está en una habitación más grande!";
+    } else if (guess > targetLocation) {
+      message.textContent = "¡El pedo está en una habitación más pequeña!";
+    } else {
+      message.textContent = "¡Felicidades! ¡Has encontrado el pedo invisible en la habitación " + targetLocation + "!";
+      message.style.color = "green";
+      guessInput.disabled = true;
+    }
+  }
+
+  guessInput.value = "";
+  guessInput.focus();
 }
 
   
