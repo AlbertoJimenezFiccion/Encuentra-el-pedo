b3010b5ce7cde1e4d11d0c7dd2157333c8397fb1
AlbertoJimenezFiccion <138886381+AlbertoJimenezFiccion@users.noreply.github.com>
2023-07-07 19:09:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,22 +12,23 @@ function checkGuess() {
     errorSound.play();
   }
 
-  if (isNaN(guess) || guess < 1 || guess > 100) {
-    message.textContent = "Por favor, ingresa un número válido entre 1 y 100.";
-    playErrorSound();
-  } else {
-    attempts++;
+ if (isNaN(guess) || guess < 1 || guess > 100) {
+  message.textContent = "Por favor, ingresa un número válido entre 1 y 100.";
+} else {
+  attempts++;
 
-    if (guess < targetLocation) {
-      message.textContent = "¡El pedo está en una habitación más grande!";
-    } else if (guess > targetLocation) {
-      message.textContent = "¡El pedo está en una habitación más pequeña!";
-    } else {
-      message.textContent = "¡Felicidades! ¡Has encontrado el pedo invisible en la habitación " + targetLocation + "!";
-      message.style.color = "green";
-      guessInput.disabled = true;
-    }
+  if (guess < targetLocation) {
+    message.textContent = "¡El pedo está en una habitación más grande!";
+  } else if (guess > targetLocation) {
+    message.textContent = "¡El pedo está en una habitación más pequeña!";
+  } else {
+    message.textContent = "¡Felicidades! ¡Has encontrado el pedo invisible en la habitación " + targetLocation + "!";
+    message.style.color = "green";
+    guessInput.disabled = true;
+    playErrorSound(); // Reproducir el sonido de error
   }
+}
+
 
   guessInput.value = "";
   guessInput.focus();
