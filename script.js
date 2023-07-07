3b1aeee7af857354f6d43c82bc1c2c2ee734365c
AlbertoJimenezFiccion <138886381+AlbertoJimenezFiccion@users.noreply.github.com>
2023-07-07 16:38:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ function checkGuess() {
   var guessInput = document.getElementById("guessInput");
   var message = document.getElementById("message");
   var guess = parseInt(guessInput.value);
+  var errorSound = new Audio("wet-fart-6139.mp3"); 
+
+function playErrorSound() {
+  errorSound.play();
+}
+
   
   if (isNaN(guess) || guess < 1 || guess > 100) {
-    message.textContent = "Por favor, ingresa un número válido entre 1 y 100.";
-  } else {
-    attempts++;
+  message.textContent = "Por favor, ingresa un número válido entre 1 y 100.";
+  playErrorSound();
+}
+
   
     if (guess < targetLocation) {
       message.textContent = "¡El pedo está en una habitación más grande!";
